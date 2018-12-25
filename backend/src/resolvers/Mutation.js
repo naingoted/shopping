b1ab72995b76e80f51611a04f5d133c1cc40606a
naingoted <boshan55@gmail.com>
2018-12-26 01:24:18 +0800

    added update mutation and single item query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,8 +10,20 @@ const mutations = {
         );
         console.log(item);
         return item;
-    }
-
+    }, 
+    updateItem(parent, args, ctx, info) {
+        const updates = {...args};
+        delete updates.id;
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id
+                }
+            },
+            info
+        )
+    },   
 };
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
 // default add custom logic later\
 items: forwardTo('db'),
+item: forwardTo('db'),
 
 };
 
